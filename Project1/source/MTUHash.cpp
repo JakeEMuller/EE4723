b817f861f162c7bfa0223ec7f74c346972256c1a
JakeEMuller <44328624+JakeEMuller@users.noreply.github.com>
2023-02-05 16:16:37 -0500

    Added File in and output functions

--- Project1/source/MTUHash.cpp
+++ Project1/source/MTUHash.cpp
@@ -1,5 +1,4 @@
-//Joan
-#define BLOCK_MULTIPLE 32
+#define BLOCK_LENGTH 32
 #define EXPANSION_TABLE_OUTPUT 48
 #define S_BOX_INPUT 6
 #define S_BOX_OUTPUT 4
@@ -9,14 +8,14 @@
 // --------------------------
 // Expansion table
 // --------------------------
-int expansionTable[8][6] = {{ 32,  1,  2,  3,  4,  5},
-                            {  4,  5,  6,  7,  8,  9},
-                            {  8,  9, 10, 11, 12, 13},
-                            { 12, 13, 14, 15, 16, 17},
-                            { 16, 17, 18, 19, 20, 21},
-                            { 20, 21, 22, 23, 24, 25},
-                            { 24, 25, 26, 27, 28, 29},
-                            { 28, 29, 30, 31, 32,  1}}; 
+int expansionTable[48] =   { 32,  1,  2,  3,  4,  5,
+                              4,  5,  6,  7,  8,  9,
+                              8,  9, 10, 11, 12, 13,
+                             12, 13, 14, 15, 16, 17,
+                             16, 17, 18, 19, 20, 21,
+                             20, 21, 22, 23, 24, 25,
+                             24, 25, 26, 27, 28, 29,
+                             28, 29, 30, 31, 32,  1}; 
 
 // --------------------------
 // Substitution table
@@ -26,6 +25,77 @@ int substitutionTable[4][16] = {{ 14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12
                                 {  4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0},
                                 { 15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13}};
 
+
+// -----------------------------
+// File Input
+// -----------------------------
+char** getInputFile(char* fileName, int* NumBlocks){
+    FILE* fp;
+
+    fp = fopen(fileName, "r+");
+
+    // get file size
+    fseek(fp, 0L, SEEK_END);
+    int sz = ftell(fp);
+    rewind(fp);
+
+    if((sz % BLOCK_LENGTH) != 0){
+        printf("ERROR: file size is not multiple of 32 (sz % 32 = %d)\n", (sz % 32));
+    }
+
+    // calculate and store num of blocks
+    *NumBlocks = (sz / BLOCK_LENGTH);
+
+    // allocate size for block array
+    char** result = (char**) malloc(sizeof(*result)* (*NumBlocks) );
+
+    for(int i = 0; i < *NumBlocks; i++){
+        
+        //allocate size for block
+        result[i] = (char*) malloc(sizeof(char) * BLOCK_LENGTH);
+        for(int j = 0; j < BLOCK_LENGTH; j++){
+            //get character
+            char character = fgetc(fp);
+
+            //printf("Character: %c \n", character);
+            //printf("HEX: %x \n", character);
+            //check if EOF 
+            if (character == EOF){
+
+                printf("ERROR: EOF Reached at Unexpected Time \n");
+                return 0;
+            // check if not 0 or 1
+            }else if(character != '0' && character != '1' ){
+                
+                printf("ERROR: Non 0 or 1 character detected\n");
+                return 0;
+            }
+
+            // put character in result
+            result[i][j] = character;
+            
+        }
+    }
+
+    fclose(fp);
+    return result;
+
+}
+
+
+// -----------------------------
+// File Output 
+// -----------------------------
+
+void createOutputFile(char* outputFileName, char* output){
+    FILE *fp;
+    fp = fopen(outputFileName, "w+");
+    for(int i = 0; i < BLOCK_LENGTH; i++){
+        fprintf(fp, "%c", output[i]);
+    }
+    fclose(fp);
+    return;
+}
 
 // --------------------------
 // Expansion Function
@@ -39,7 +109,7 @@ int* expansionFunction(int input[48]){
 // Substitution Function
 // --------------------------
 
-int* substitutionFunction(int input[6]){
+int* substitutionFunction(int input[S_BOX_INPUT]){
 
 }
 
@@ -68,9 +138,6 @@ int* MTUHash(int* bitstream){
     
 }
 
-// --------------------------
-// Avalanche Effect Function
-// --------------------------
 
 
 // --------------------------
@@ -79,12 +146,32 @@ int* MTUHash(int* bitstream){
 int main(){
 
     //get input
+    printf("hello world \n");
+    int numBlocks = 0;  
+    char** blocks = getInputFile("Hashin(96 bit).txt", &numBlocks);
 
-    //call mtu hash
-    print("hello world");
-    //output
+
+    printf("Number of blocks: %d \n", numBlocks);
+    printBlocks(blocks, numBlocks);
+    
+    createOutputFile("Hashout_TEST1.txt", blocks[0]);
 
     //exit
     return 0;
 
+}
+
+
+
+// debug functions
+
+void printBlocks(char** blocks, int numBlocks){
+    
+    for(int i = 0; i < numBlocks; i++){
+        printf("Block %d: ", i);
+        for(int j = 0; j < 32; j++){
+            printf("%c", blocks[i][j]);
+        }
+        printf("\n");
+    }       
 }
--- Project1/source/MTUHash.h
+++ Project1/source/MTUHash.h
@@ -3,11 +3,16 @@
 #include <iostream>
 #include <stdint.h>
 
+
 using namespace std;
 
-int expansionTable[8][6];
+//int expansionTable[8][6];
 
-int substitutionTable[4][16];
+//int substitutionTable[4][16];
+
+char** getInputFile(char* fileName, int* NumBlocks);
+
+
 
 int* expansionFunction(int input[48]);
 
@@ -18,3 +23,7 @@ int* XOR_Function(int* values[32], int blocks);
 int* final_XOR_Function(int* values[32], int blocks);
 
 int* MTUHash(int* bitstream);
+
+// debug functions
+
+void printBlocks(char** blocks, int numBlocks);
